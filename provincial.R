b8a6eedd51d6fa70907ff756c07a128aef8ab85e
arinwongprommoon <bht9216@gmail.com>
2020-01-02 13:57:55 +0000

    spring cleaning commit

--- provincial.R
+++ provincial.R
@@ -1,4 +1,7 @@
 library(dplyr)
+library(maps)
+library(mapdata)
+library(sp)
 
 votedata <- read.csv("FullReporting_ECT_20190328_District_level_EN.csv")
 parties <- as.list(levels(votedata$Party))
@@ -16,3 +19,16 @@ provincevotespercent <- merge(provincepop, provincevotes, by = "Province")
 provincevotespercent$SumVotes.y <- 100*provincevotespercent$SumVotes.y/provincevotespercent$SumVotes.x
 provincevotespercent$SumVotes.x <- NULL
 colnames(provincevotespercent) <- c("Province", "Party", "Percent")
+
+# make map
+# this code makes inaccurate maps, ignore it.
+# also the province names and those from gadm have different spelling, so that
+# might contribute to the error
+
+gadm <- readRDS("gadm36_THA_1_sp.rds")
+shadesofgrey <- colorRampPalette(c("grey100", "grey0"))
+colours <- shadesofgrey(100)
+pt <- provincevotespercent[provincevotespercent$Party == "Bhumjaithai Party",]
+#regions <- pt$Province
+votepercents <- pt$Percent
+plot(gadm, border = 'darkgrey', col = colours[votepercents])
